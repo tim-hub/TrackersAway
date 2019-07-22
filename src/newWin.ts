6dbd6355982d87986d63ba6c9563662c287f1da5
tim-hub <tim_w@outlook.com>
2019-07-22 20:16:16 +1200

    get a workable reader

--- src/newWin.ts
+++ src/newWin.ts
@@ -1,20 +1,28 @@
 // const { Notification } = require("electron");
-const fs = require("fs");
+// const fs = require("fs");
+const { readHosts } = require("./utils/readHosts");
 
-const readHosts = async hosts => {
-  return await fs.promises.readFile(hosts);
-};
+// const readHosts = async hosts => {
+//   return await fs.promises.readFile(hosts);
+// };
+
+// const readHosts = async (hostsPath = "/etc/hosts") => {
+//     return await fs.promises.readFile(hostsPath);
+// };
 
 const btn = document.getElementById("new-window");
 
 btn.addEventListener("click", event => {
   console.log("hi");
-  console.log(readHosts("/etc/hosts"));
+  readHosts("/etc/hosts")
+    .then(r => console.log(r.toString()))
+    .catch(e => console.log(e));
+  // console.log(readHosts());
 });
-let myNotification = new Notification("Title", {
-  body: "Lorem Ipsum Dolor Sit Amet"
-});
-
-myNotification.onclick = () => {
-  console.log("Notification clicked");
-};
+// let myNotification = new Notification("Title", {
+//   body: "Lorem Ipsum Dolor Sit Amet"
+// });
+//
+// myNotification.onclick = () => {
+//   console.log("Notification clicked");
+// };
--- src/utils/io.ts
+++
@@ -1,5 +0,0 @@
-import * as fs from "fs";
-
-export const readHosts = async hosts => {
-  return await fs.promises.readFile(hosts);
-};
