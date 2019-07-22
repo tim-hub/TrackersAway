26cb3142039fbc91eb54495e4b84ba912a22614c
tim-hub <tim_w@outlook.com>
2019-07-23 08:22:27 +1200

    try to write to file

--- src/newWin.ts
+++ src/newWin.ts
@@ -1,23 +1,46 @@
-// const { Notification } = require("electron");
-// const fs = require("fs");
-const { readHosts } = require("./utils/readHosts");
+const axios = require("axios");
+const { readHosts } = require("./utils/io");
 
-// const readHosts = async hosts => {
-//   return await fs.promises.readFile(hosts);
-// };
-
-// const readHosts = async (hostsPath = "/etc/hosts") => {
-//     return await fs.promises.readFile(hostsPath);
-// };
+const hosts =
+  "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
 
 const btn = document.getElementById("new-window");
 
 btn.addEventListener("click", event => {
   console.log("hi");
-  readHosts("/etc/hosts")
-    .then(r => console.log(r.toString()))
-    .catch(e => console.log(e));
-  // console.log(readHosts());
+  // readHosts("/etc/hosts")
+  //   .then(r => console.log(r.toString()))
+  //   .catch(e => console.log(e));
+  axios.get(hosts).then(data => {
+    console.log(data.data);
+    // const fs = require("fs");
+    // fs.writeFile("/tmp/hosts", data.data, err => {
+    //   if (err) {
+    //     return console.log(err);
+    //   }
+    //   console.log("The file was saved!");
+    // });
+    const lines = data.data.split(/\r?\n/);
+
+    var sudo = require("sudo-prompt");
+    var options = {
+      name: "Write to Hosts"
+    };
+
+    const getCmd = line => `echo ${line} >> /tmp/tmp111 && `;
+    const writing = `${lines.reduce((a, b) => {
+      return getCmd(a) + "" + getCmd(b);
+    })}`;
+
+    sudo.exec(
+      writing.substring(writing.length - 3, writing.length),
+      options,
+      function(error, stdout, stderr) {
+        if (error) throw error;
+        console.log("stdout: " + stdout);
+      }
+    );
+  });
 });
 // let myNotification = new Notification("Title", {
 //   body: "Lorem Ipsum Dolor Sit Amet"
--- src/utils/io.js
+++ src/utils/io.js
@@ -1,7 +1,10 @@
 const fs = require("fs").promises;
 
+
 const readHosts = async (hosts = "/etc/hosts") => {
   return await fs.readFile(hosts);
 };
 
-module.exports = { readHosts };
+
+
+module.exports = { readHosts: readHosts };
