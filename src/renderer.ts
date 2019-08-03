c59383924847e18937c3aef043000f09efa5e025
tim-hub <tim_w@outlook.com>
2019-08-03 14:02:35 +1200

    clean the codes

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,7 +1,5 @@
 const fs = require("fs").promises;
 const axios = require("axios");
-// import axios from "axios";
-const { readHosts } = require("./utils/io");
 
 const hosts =
   "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
@@ -9,19 +7,8 @@ const hosts =
 const btn = document.getElementById("new-window");
 
 btn.addEventListener("click", event => {
-  console.log("hi");
-  // readHosts("/etc/hosts")
-  //   .then(r => console.log(r.toString()))
-  //   .catch(e => console.log(e));
+  console.log("button clicked");
   axios.get(hosts).then(data => {
-    // console.log(data.data);
-    // const fs = require("fs");
-    // fs.writeFile("/tmp/hosts", data.data, err => {
-    //   if (err) {
-    //     return console.log(err);
-    //   }
-    //   console.log("The file was saved!");
-    // });
     const lines = data.data.split(/\r?\n/);
     console.log(lines);
 
@@ -29,19 +16,7 @@ btn.addEventListener("click", event => {
     var options = {
       name: "Write to Hosts"
     };
-
-    const getCmd = line => `echo ` + line + ` >> /tmp/tmp111 && `;
-    // console.log(getCmd("a"));
-    let cmds = "";
-
-    lines.forEach(x => {
-      cmds += getCmd(x);
-    });
-
-    console.log(cmds.substring(0, cmds.length - 3));
-
     const hostsPath = "/opt/hosts";
-
     sudo.exec(`chmod 777 ${hostsPath}`, options, (error, stdout, stderr) => {
       if (error) throw error;
       console.log("stdout: " + stdout);
@@ -57,23 +32,5 @@ btn.addEventListener("click", event => {
         console.log("stderr: " + stderr);
       });
     });
-
-    // sudo.exec(cmds.substring(0, cmds.length - 3), options, function(
-    //   error,
-    //   stdout,
-    //   stderr
-    // ) {
-    //   if (error) throw error;
-    //   console.log("err: " + error);
-    //   console.log("stdout: " + stdout);
-    //   console.log("stderr: " + stderr);
-    // });
   });
 });
-// let myNotification = new Notification("Title", {
-//   body: "Lorem Ipsum Dolor Sit Amet"
-// });
-//
-// myNotification.onclick = () => {
-//   console.log("Notification clicked");
-// };
