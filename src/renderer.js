891b28361975eb1510da361c0d290f707b6af216
tim-hub <tim_w@outlook.com>
2019-08-11 15:58:12 +1200

    get an elegant way to ask permission

--- src/renderer.js
+++ src/renderer.js
@@ -1,14 +1,59 @@
 const {logger} = require("./utils/logger");
 const {main} = require('./utils/io');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const sudo = require("sudo-prompt");
 
+const addPermission = async() => {
+  console.log('change permission');
+  const { stdout, stderr } = await exec('chmod +x utils/fs-writer/index-macos');
+  return new Promise((resolve, reject)=>{
+    if (stderr) {
+      console.error(`error: ${stderr}`);
+      reject();
+    }
+    console.log(`Number of files ${stdout}`);
+    resolve();
+  })
+
+};
+
+const writeToFile = async (content)=>{
+  console.log('write to file');
+  var options = {
+    name: "Write to Hosts"
+  };
+
+  return new Promise((resolve, reject)=>{
+    sudo.exec(`utils/fs-writer/index-macos ${content} ${'/opt/hosts'}`,
+      options,
+      (error, stdout, stderr) => {
+        if (error){
+          reject();
+          throw error;
+        }
+        console.log("stdout: " + stdout);
+        console.log("stderr: " + stderr);
+        resolve();
+      }
+    )
+  })
+};
 
 
 const btn = document.getElementById("new-window");
 
-btn.addEventListener("click", event => {
+btn.addEventListener("click", async (event) => {
   logger.debug("button clicked");
 
-  main()
+  // main()
+
+  await addPermission();
+  await writeToFile('asdjjjdf');
+
+
+
+
 
   // axios.get(hostsDefaultUrl).then(data => {
   //   const lines = data.data.split(/\r?\n/);
