8d24042eed7616b04501bea7dcc480ebf52519f5
tim-hub <tim_w@outlook.com>
2020-02-17 21:46:52 +1300

    add more states in store to make it more friendly

--- src/renderer.js
+++ src/renderer.js
@@ -1,14 +1,28 @@
 const {logger} = require('./utils/logger');
 const {main} = require('./index');
-const {store, toggleLoading} = require('./store');
+const {
+  store,
+  toggleLoading,
+  updateFetchingState,
+  FETCHING_STATE,
+} = require('./store');
 
 const valueEl = document.getElementById('apply-button');
+const logEl = document.getElementById('log-area');
 
 const render = () => {
   if (store.getState().ui.isLoading) {
     valueEl.classList.add('is-loading');
+    logEl.innerHTML='Fetching and updating the hosts';
   } else {
     valueEl.classList.remove('is-loading');
+    if (store.getState().fetching.state === FETCHING_STATE.error) {
+      logEl.innerHTML = 'ERROR,' +
+        'Sudo permission is required to update hosts file';
+    }
+    if (store.getState().fetching.state === FETCHING_STATE.done) {
+      logEl.innerHTML='Done';
+    }
   }
 };
 
@@ -20,11 +34,14 @@ const btn = document.getElementById('apply-button');
 btn.addEventListener('click', async (event) => {
   logger.debug('button clicked');
   toggleLoading();
+  updateFetchingState(FETCHING_STATE.fetching);
   try {
     await main('/etc/hosts');
     toggleLoading();
+    updateFetchingState(FETCHING_STATE.done);
   } catch (e) {
     toggleLoading();
+    updateFetchingState(FETCHING_STATE.error);
     logger.error(e);
   }
 });
